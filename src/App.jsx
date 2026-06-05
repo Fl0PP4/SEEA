275bcf8d3325c66122aeea99ebec8dc18025511e
Fl0PP4 <160527272+Fl0PP4@users.noreply.github.com>
2026-06-05 19:02:32 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Home from './components/Home/home'
+import Login from './components/login/login'
+
+
+function App() {
+  return (
+    <>
+    <Login/>
+   <Home/>
+    </>
+  
+  )
+}
+
+export default App
