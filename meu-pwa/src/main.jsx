7c3f108327384071e005aa25003f5cb83821fae5
Floppa <amaralismael11@gmail.com>
2026-06-12 16:56:00 -0300

    mini update

--- meu-pwa/src/main.jsx
+++ meu-pwa/src/main.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx'
 
 import { registerSW } from 'virtual:pwa-register'
 import { BrowserRouter } from 'react-router-dom'
+import 'leaflet/dist/leaflet.css';
 
 registerSW({ immediate: true })
 
